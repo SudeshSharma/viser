50dd0c7e0db4eeae4c6977a757ed2d88aba7fa00
Sudesh Sharma <Sudesh.Sharma@visertechnosys.local>
2019-08-19 15:16:04 +0530

    Updates

---
+++ precache-manifest.c6bf699042b293c0d0a3ae6cd83b7992.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de03d9a542bec1b1df4b720470b7596",
+    "url": "/viser/index.html"
+  },
+  {
+    "revision": "c939eff8c95b1b9f3c46",
+    "url": "/viser/static/css/main.95bbe8fd.chunk.css"
+  },
+  {
+    "revision": "b8fcb953362afe74cddf",
+    "url": "/viser/static/js/2.9f8614f6.chunk.js"
+  },
+  {
+    "revision": "c939eff8c95b1b9f3c46",
+    "url": "/viser/static/js/main.4226a3a1.chunk.js"
+  },
+  {
+    "revision": "3db8e8d93a9830b8c67f",
+    "url": "/viser/static/js/runtime~main.912abb92.js"
+  },
+  {
+    "revision": "c8c07a6c8c28acb46be82cf7d32a5cc2",
+    "url": "/viser/static/media/GST1.c8c07a6c.jpg"
+  },
+  {
+    "revision": "ea9964aa530bfaf1e67dec823ea865ac",
+    "url": "/viser/static/media/GST2.ea9964aa.jpg"
+  },
+  {
+    "revision": "744e0dc1d542db708a6888276f3a9d9d",
+    "url": "/viser/static/media/GST3.744e0dc1.jpg"
+  },
+  {
+    "revision": "bc7332ca178fc85b9977883bedd535cf",
+    "url": "/viser/static/media/Tally1.bc7332ca.pdf"
+  },
+  {
+    "revision": "078b0c87d46a500e085528723566cae2",
+    "url": "/viser/static/media/Tallyerp.078b0c87.png"
+  },
+  {
+    "revision": "32080975a6375d5ecc18c3a9fcdf6c93",
+    "url": "/viser/static/media/UTGST.32080975.jpg"
+  },
+  {
+    "revision": "18a0808d2bb928924226fd601e8b51fd",
+    "url": "/viser/static/media/auth1.18a0808d.jpeg"
+  },
+  {
+    "revision": "dde78fd48ce0cbb42fb986c33229dda1",
+    "url": "/viser/static/media/backheader.dde78fd4.png"
+  },
+  {
+    "revision": "85eabf27c69c342fb8ad19f9d0db0fa5",
+    "url": "/viser/static/media/blog3.85eabf27.jpg"
+  },
+  {
+    "revision": "5d66461cab87a34ff284a6dbd8354eed",
+    "url": "/viser/static/media/erp_image.5d66461c.png"
+  },
+  {
+    "revision": "26631a4043b14dff84180bdf51c3cacb",
+    "url": "/viser/static/media/instagram.26631a40.png"
+  },
+  {
+    "revision": "8987232f80cbb46dbb3758ac787be649",
+    "url": "/viser/static/media/pricing.8987232f.jpg"
+  },
+  {
+    "revision": "f79e09e4de429225c696088d9dbf904e",
+    "url": "/viser/static/media/regi.f79e09e4.jpeg"
+  },
+  {
+    "revision": "3c32a2b8face91a29f15af41fb75659d",
+    "url": "/viser/static/media/regiback.3c32a2b8.jpg"
+  },
+  {
+    "revision": "348a51196c4d2b458b4c607c017b8c87",
+    "url": "/viser/static/media/tally18.348a5119.jpg"
+  },
+  {
+    "revision": "a8a826f439472399423ad79f0acc1b1a",
+    "url": "/viser/static/media/tally2.a8a826f4.png"
+  },
+  {
+    "revision": "aa9b64a1bee20faff516cf5939490dd5",
+    "url": "/viser/static/media/tally65.aa9b64a1.jpg"
+  },
+  {
+    "revision": "dc14beec689e4bfef4348e4cece978e4",
+    "url": "/viser/static/media/tt.dc14beec.jpg"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/viser/static/media/twitter.0251d8ee.png"
+  },
+  {
+    "revision": "fc601ba58f923366416bd69b8be6cca4",
+    "url": "/viser/static/media/whatsapp.fc601ba5.png"
+  }
+]);
